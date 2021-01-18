51e2f855f6ff0fa5c407c3c4bc8ef18fbe59c090
zhouchaoyi <zhouchaoyi@sohu.com>
2021-01-18 16:11:12 +0800

    修改配置文件

--- src/config/env.js
+++ src/config/env.js
@@ -1,14 +1,12 @@
 // 公共地址
 
+let baseUrl = 'http://localhost:8080/learn';// 开发服务器
+let baseFileUrl = 'http://localhost:8088' //开发服务器
 
+//  let baseUrl = 'https://zhjy.917tou.com/learn' //测试服务器
+//  let baseFileUrl = 'https://zhjy.917tou.com' //测试服务器
 
- let baseUrl = 'https://zhjy.917tou.com/learn' //测试服务器
-
- let baseFileUrl = 'https://zhjy.917tou.com' //测试服务器
-
-//  let baseUrl = 'https://zhjy.917tou.com/learn';// 开发服务器 npm install
-// let baseUrl = 'https://www.bolekids.com/leran' //线上服务器
-
+// let baseUrl = 'https://www.bolekids.com/learn' //线上服务器
 // let baseFileUrl = 'https://www.bolekids.com' //线上服务器
 
 let imgUrl = baseFileUrl+'/file-local-1.0.0/img/upload'//上传图片接口
